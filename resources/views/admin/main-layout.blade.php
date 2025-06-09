f517baa4237faa6ffffccfa1306444d58db5633a
Plat0ne <alessia.brina@edu.unife.it>
2025-06-09 13:43:59 +0200

    modificato il font (temporaneo)

--- resources/views/admin/main-layout.blade.php
+++ resources/views/admin/main-layout.blade.php
@@ -12,15 +12,18 @@
 
          <!-- Font personalizzati per questo template-->
         <link href="{{ asset('admin_assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-        <link
-            href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-            rel="stylesheet">
+
+        <!-- href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+            rel="stylesheet" -->
         <!-- Stili personalizzati per questo template-->
         <link href="{{ asset('admin_assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
 
 
         <!-- per il date range picker -->
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+
+        <!-- Font personalizzati per questo template-->
+        @vite(['resources/css/app.css', 'resources/css/font.css', 'resources/js/app.js'])
 
     </head>
 
